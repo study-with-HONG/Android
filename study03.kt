f512518bb16df6f8bf18ac304caca6105beb5f91
study-with-HONG <55612285+study-with-HONG@users.noreply.github.com>
2022-03-13 21:35:02 +0900

    Update study03.kt

--- study03.kt
+++ study03.kt
@@ -11,16 +11,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        val toast = Toast.makeText(this.applicationContext, "클릭", Toast.LENGTH_SHORT)
-        toast.show()
-
         when(v?.id){
             R.id.click -> {
                 var img = findViewById<ImageView>(R.id.whiteStar)
 
                 img.visibility = if(img.visibility == View.VISIBLE){
+                    val toast = Toast.makeText(this.applicationContext, "사라졌다", Toast.LENGTH_SHORT)
+                    toast.show()
                     View.INVISIBLE // 이미지 없애기
                 } else{
+                    val toast = Toast.makeText(this.applicationContext, "나타났다", Toast.LENGTH_SHORT)
+                    toast.show()
                     View.VISIBLE // 이미지 보여주기
                 }
             }
